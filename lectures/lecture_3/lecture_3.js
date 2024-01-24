38038d9ba29eb9ec59470c002a37e9306d15cadc
elit <elityaev@yandex.ru>
2024-01-24 14:28:47 +0300

    added sem 3

---
+++ lectures/lecture_3/lecture_3.js
@@ -0,0 +1,19 @@
+function helloName(name) {
+    console.log(name);
+}
+
+helloName('Alex');
+
+let count = 5;
+
+function counter() {
+    return count++;
+}
+
+counter();
+
+console.log(count);
+
+function sayHello() {
+    alert('Привет!');
+}
