8fb68115ba54a658deb20d971749d6d0ce92c45f
elit <elityaev@yandex.ru>
2024-02-02 07:40:46 +0300

    added homework 5

---
+++ homeworks/homework_5/4.js
@@ -0,0 +1,41 @@
+"use strict";
+
+/*
+1. Необходимо вывести в консоль массив продуктов в котором есть хоть одна
+фотография, используя метод filter. Исходные данные - массив products.
+2. Необходимо отсортировать массив products используя метод sort по цене,
+начиная с самой маленькой, заканчивая самой большой ценой, после чего вывести
+отсортированный массив в консоль.
+Если сложно работать с методами массива, то можно сделать и обычным циклом.
+*/
+
+const products2 = [
+  {
+    id: 3,
+    price: 127,
+    photos: ["1.jpg", "2.jpg"],
+  },
+  {
+    id: 5,
+    price: 499,
+    photos: [],
+  },
+  {
+    id: 10,
+    price: 26,
+    photos: ["3.jpg"],
+  },
+  {
+    id: 8,
+    price: 78,
+  },
+];
+
+const productsWithPhoto = products2.filter((product) => {
+  if (product.photos && product.photos.length > 0) return true
+  return false
+})
+console.log(productsWithPhoto);
+
+const productsSort = products2.sort((a, b) => a.price - b.price);
+console.log(productsSort);
